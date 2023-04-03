ce46973a07b6b81702893ca1388006d8a371d0c9
Miguel Angel <angelbetancur.zapata@gmail.com>
2023-04-02 20:36:49 -0500

    added script for the POO structure

---
+++ scripts/products.class.js
@@ -0,0 +1,9 @@
+export default class Product{
+    constructor(id, name, price, stock, image) {
+      this.id = id;
+      this.name = name;
+      this.price = price;
+      this.stock = stock;
+      this.image = image;
+    }
+  }
